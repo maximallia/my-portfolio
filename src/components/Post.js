594ec1fe8fc21beba42bbf099dd76f06960853a5
maximallia <daniel.t.ying@gmail.com>
2022-01-14 23:21:40 -0500

    update client

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import{Link} from "react-router-dom"
-import sanityClient from "/Users/DY/Desktop/folders/portfolio/src/client.js";
+import sanityClient from "/Users/DY/Desktop/folders/portfolio/src/client.js/";
 
 
 export default function Post(){
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
-import sanityClient from "/Users/DY/Desktop/folders/portfolio/src/client.js";
+import sanityClient from "/Users/DY/Desktop/folders/portfolio/src/client.js/";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react"
 
